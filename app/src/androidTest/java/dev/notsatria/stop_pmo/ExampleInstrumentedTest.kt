7f991051786fa315a4196d79ef14b2aacb89d8ef
notsatria <buanasatriaa@gmail.com>
2025-08-26 19:24:40 +0700

    Init project

---
+++ app/src/androidTest/java/dev/notsatria/stop_pmo/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package dev.notsatria.stop_pmo
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("dev.notsatria.stop_pmo", appContext.packageName)
+    }
+}
