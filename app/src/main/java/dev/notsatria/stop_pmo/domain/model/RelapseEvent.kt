43871f2feb0b335cc5031118079930ae8bb5466d
notsatria <buanasatriaa@gmail.com>
2025-08-27 20:26:02 +0700

    feat: Setup domain entity RelapseEvent.kt

---
+++ app/src/main/java/dev/notsatria/stop_pmo/domain/model/RelapseEvent.kt
@@ -0,0 +1,7 @@
+package dev.notsatria.stop_pmo.domain.model
+
+data class RelapseEvent(
+    val id: Int,
+    val occurredAt: String, // ISO 8601 format date-time string
+    val note: String? // Optional notes about the relapse event
+)
